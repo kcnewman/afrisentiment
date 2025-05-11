159fd53fbf187abe299184f5e8e7330d4ed9670c
kcnewman <newmankelvin14@gmail.com>
2025-05-11 18:55:12 +0000

    Add initial project files and setup configuration

---
+++ src/utils.py
@@ -0,0 +1,25 @@
+def clean_text(text):
+    "Clean text, lowercase punctuation"
+    pass
+
+def tokenize(text):
+    "Split cleaned text into tokens."
+    pass
+
+def remove_stopwords(tokens, stopewords_set):
+    "remove stop words"
+    pass
+
+class preprocess_text(text):
+    "A wrapper of the funcs above"
+    pass
+
+def build_vocab(corpus):
+    "extract unique words from corpus"
+    pass
+
+def count_word_freqs_by_class(texts,labels):
+    'return class-wise word freqs dict'
+    pass
+
+def count_word_frequen
